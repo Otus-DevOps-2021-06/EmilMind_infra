ac9586beb0366c2483f09b1ce63997a4de20f6b3
Emil Mindubaev <emindubaev@express42.com>
2021-06-18 14:20:41 +0300

    init
    Создал новую ветку, перенес файлы с прошлого задания

--- config-scripts/deploy.sh
+++ config-scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y git
+apt-get update
+apt-get install -y git
 cd /home/yc-user
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
